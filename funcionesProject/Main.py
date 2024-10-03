a5d1330bb7a4532cc06740f23e4b9281e658ea40
Erick Santiago <erickmunozz44@gmail.com>
2024-10-03 14:53:19 -0500

    se crea el documento main con el archivo hola mundo

---
+++ funcionesProject/Main.py
@@ -0,0 +1,14 @@
+#Definicion de una funcion
+def my_funtion():
+    print("Hola from a function")
+
+
+
+#Invocar la funcion
+my_funtion()
+
+def mostrarNombre(nombre):
+    print("su nombres es:"+nombre)
+
+nom = input("Ingrese su nombre")
+mostrarNombre(nom)
